abfd97912f90cf0c0b59d9c4f3ade1ee4246f14c
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-03 23:02:12 +0200

    added stack navigation

--- App.js
+++ App.js
@@ -2,20 +2,39 @@
 import { StatusBar } from "expo-status-bar";
 
 // React Native
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Morra Cinese Components Library
-import { SpaceBackground } from "morra-cinese-components";
+// import { SpaceBackground } from "morra-cinese-components";
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Images
 const backgroundImage = require("./assets/images/welcome/background.png");
 
+// Screens
+import Welcome from "./screens/welcome/Welcome";
+import Game from "./screens/game/Game";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SpaceBackground bgImage={backgroundImage} />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style={"auto"} hidden />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Group screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="Game" component={Game} />
+          </Stack.Group>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
@@ -23,7 +42,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
